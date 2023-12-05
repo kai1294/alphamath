15a24ea0bc8a7aa69c7c075914b1143bb521cac1
Dennis <43997085+TheAlan404@users.noreply.github.com>
2023-12-05 08:48:50 +0300

    add previous dark mode back in

--- src/index.js
+++ src/index.js
@@ -12,8 +12,25 @@ import i18n from "./i18n";
 import '@mantine/core/styles.css';
 import 'mantine-contextmenu/styles.css';
 
+const theme = createTheme({
+  colors: {
+    dark: [
+      '#C1C2C5',
+      '#A6A7AB',
+      '#909296',
+      '#5c5f66',
+      '#373A40',
+      '#2C2E33',
+      '#25262b',
+      '#1A1B1E',
+      '#141517',
+      '#101113',
+    ],
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MantineProvider defaultColorScheme='dark'>
+root.render(<MantineProvider defaultColorScheme='dark' theme={theme}>
         <I18nextProvider i18n={ i18n }>
             <Notifications />
             <ModalsProvider>
